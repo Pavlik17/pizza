422caef9f154ed763b903330b0d61dd09068ecfc
Pavel_17 <pavelragliup@gmail.com>
2024-07-06 05:57:49 +0300

    адаптив для планшета, частично мобилы корзины'origin/main'

--- src/components/CartElement/CartElement.vue
+++ src/components/CartElement/CartElement.vue
@@ -3,6 +3,7 @@ import MinusCartButton from "@/components/icons/MinusCartButton/MinusCartButton.
 import PlusCartButton from "@/components/icons/PlusCartButton/PlusCartButton.vue";
 import DeleteProductCartButton from "@/components/icons/DeleteProductCartButton/DeleteProductCartButton.vue";
 import {ProductDataStore} from "@/store/store.js";
+import DeleteProduct from "@/components/icons/DeleteProduct/DeleteProduct.vue";
 
 const store = ProductDataStore();
 const props = defineProps({
@@ -28,6 +29,24 @@ const props = defineProps({
     </div>
     <div class="price_product">{{props.price * props.count}} р.</div>
     <delete-product-cart-button class="delete-product-cart-button" @click="store.removeProductFromCart(props.idProduct)"/>
+  </div>
+
+  <div class="mobile-cart-element">
+    <div class="mobile-product-image">
+      <img class="mobile-image" src="../../../public/assets/pizza_cart_image.png" alt="pizza">
+    </div>
+    <p class="mobile-product-name">{{props.product_name}}</p>
+    <div class="mobile-quantity-price">
+      <div class="mobile-quantity-panel">
+        <minus-cart-button class="mobile-minus_quantity_button" @click="store.minusProductInCart(idProduct)"/>
+        <div class="mobile-quantity_products">{{props.count}}</div>
+        <plus-cart-button class="mobile-add_quantity_button" @click="store.plusProductInCart(idProduct)"/>
+      </div>
+      <div class="mobile-price_product">{{props.price * props.count}} р.</div>
+    </div>
+    <button class="mobile-delete-product-cart-button" @click="store.removeProductFromCart(props.idProduct)">
+      <delete-product></delete-product>
+    </button>
   </div>
 </template>
 
---
+++ src/components/icons/DeleteProduct/DeleteProduct.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10.0547 10.0554L5.51821 5.51898M5.51821 5.51898L0.981759 0.982535M5.51821 5.51898L10.0547 0.982535M5.51821 5.51898L0.981759 10.0554" stroke="#D3D3D3" stroke-width="1.60388" stroke-linecap="round"/>
+  </svg>
+</template>
